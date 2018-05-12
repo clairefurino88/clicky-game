6f1ced66c25262726393415a082b9afd7fc82690
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-05-12 19:39:14 -0400

    added inital functionality with clicking

---
+++ clicky-game/src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>Cat Clicky Game!</h1>
+        <p>Click on a cat to earn points, but don't click on a cat more than once!</p>
+    </div>
+);
+
+
+export default Title;
+
